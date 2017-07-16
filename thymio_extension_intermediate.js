980cfcd0ad47ca6dc3aca51e9a92f438c669dc6f
Elisa Bernardoni <elisa.bernardoni@gmail.com>
2017-07-16 13:43:55 +0200

    Update thymio_extension_intermediate.js

--- thymio_extension_intermediate.js
+++ thymio_extension_intermediate.js
@@ -126,16 +126,14 @@
         source.addEventListener('message', function(e) {
 
             eventData = e.data.split(" ");
-
-            if (eventData[0] == "R_state_update") {
+	    connected = 2;
+            
+	    if (eventData[0] == "R_state_update") {
                 cachedValues = eventData;
-                	console.log("cached "+cachedValues);
-                connected = 2;
             } else {
                 if (DEBUG) {
                     console.log("emitted " + eventData)
                 }
-                connected = 1;
             }
 
 
@@ -172,7 +170,7 @@
     }
 
 	/**
-     * TEST - The function sends Aesl for Thtmio to asebahttp bridge
+     * The function sends the code of thymio_motion.aesl to asebahttp bridge
      */
 
     function loadAesl() {
@@ -2239,7 +2237,6 @@ motor.right.target = event.args[0] \
                 rgb[2] = 33 - color % 33;
                 break;
         }
-        console.log(rgb);
         return rgb;
     }
 
